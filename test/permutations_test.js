b7db96ef62c8b53fb2ee3768e561198127adbadd
Benoît Baliguet <benoit.baliguet@gmail.com>
2014-02-01 23:15:07 +0100

    add skip feature

--- test/permutations_test.js
+++ test/permutations_test.js
@@ -53,5 +53,12 @@ exports['awesome'] = {
     // this should loop
     test.deepEqual(all[0], generator.next());
     test.done();
+  },
+  'generator with skip': function(test) {
+    var all = permutations.all([1, 2], 4),
+      generator = permutations.generator([1, 2], 4);
+    // skip on first index
+    test.deepEqual(all[3], generator.next(0));
+    test.done();
   }
 };
